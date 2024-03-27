3c941aef4fed4189b749b7b80125fe35b139ba59
Nivaldo21 <djnivis@gmail.com>
2024-03-27 00:39:28 -0600

    build: add empty component of vuetify

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,14 +1,18 @@
 import {createVuetify} from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import '@mdi/font/css/materialdesignicons.css' 
+import '@mdi/font/css/materialdesignicons.css';
+import { VEmptyState } from 'vuetify/labs/VEmptyState'; // Import VEmptyState
 
 export default defineNuxtPlugin((nuxt)=>{
     const vuetify = createVuetify({
+        components: {
+            ...components, // Spread existing components
+            VEmptyState, // Add VEmptyState to components
+          },
         icons: {
             defaultSet: 'mdi'
         },
-        components,
         directives 
     });
     nuxt.vueApp.use(vuetify);
