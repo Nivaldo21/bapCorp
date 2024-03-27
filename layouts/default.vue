c0770911cb0165937eed47435377996032b7c9f1
Nivaldo21 <djnivis@gmail.com>
2024-03-27 00:41:43 -0600

    feat: add nuxtPage for page routes and nuxt layout

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="d-flex justify-space-between align-center my-2 mx-5 flex-wrap">
+        <h2>Gestión de Tareas</h2>
+        <h4>Desarrolado por: Nivaldo Quiñones</h4>
+    </div>
+    <v-divider />
+    <slot></slot>
+</template>
